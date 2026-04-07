a955dd41014c4cb2b4defce0e2ebb0a97bf74560
mkpazan <mia.k.pazan@vanderbilt.edu>
2026-04-07 17:22:03 -0500

    reset, movement, and random generation
    there is some issues with the movement once the entire 4x4 grid is filled for some reason one tile just disappears. this is work in progress. sw17 is the reset, and the buttons on the fpga are the movements

--- DE2_115_TOP.vhd
+++ DE2_115_TOP.vhd
@@ -4,9 +4,8 @@ use ieee.numeric_std.all;
 
 entity DE2_115_TOP is
   generic (
-    TICKS_PER_SECOND : natural := 50_000_000  -- default for 50 MHz CLOCK_50
+    TICKS_PER_SECOND : natural := 50_000_000
   );
-
   port (
     -- Clocks
     CLOCK_50   : in std_logic;
@@ -101,17 +100,64 @@ architecture structural of DE2_115_TOP is
       Blue         : out std_logic_vector(7 downto 0)
     );
   end component;
-  
+
   component tiles_2048
-  port(
-    pixel_row    : in  std_logic_vector(10 downto 0);
-    pixel_column : in  std_logic_vector(10 downto 0);
-    tile_on      : out std_logic;
-    Red          : out std_logic_vector(7 downto 0);
-    Green        : out std_logic_vector(7 downto 0);
-    Blue         : out std_logic_vector(7 downto 0)
-  );
-end component;
+    port(
+      pixel_row    : in  std_logic_vector(10 downto 0);
+      pixel_column : in  std_logic_vector(10 downto 0);
+
+      cell00 : in std_logic_vector(3 downto 0);
+      cell01 : in std_logic_vector(3 downto 0);
+      cell02 : in std_logic_vector(3 downto 0);
+      cell03 : in std_logic_vector(3 downto 0);
+      cell10 : in std_logic_vector(3 downto 0);
+      cell11 : in std_logic_vector(3 downto 0);
+      cell12 : in std_logic_vector(3 downto 0);
+      cell13 : in std_logic_vector(3 downto 0);
+      cell20 : in std_logic_vector(3 downto 0);
+      cell21 : in std_logic_vector(3 downto 0);
+      cell22 : in std_logic_vector(3 downto 0);
+      cell23 : in std_logic_vector(3 downto 0);
+      cell30 : in std_logic_vector(3 downto 0);
+      cell31 : in std_logic_vector(3 downto 0);
+      cell32 : in std_logic_vector(3 downto 0);
+      cell33 : in std_logic_vector(3 downto 0);
+
+      tile_on : out std_logic;
+      Red     : out std_logic_vector(7 downto 0);
+      Green   : out std_logic_vector(7 downto 0);
+      Blue    : out std_logic_vector(7 downto 0)
+    );
+  end component;
+
+  component movement_2048
+    port(
+      clk       : in  std_logic;
+      reset_n   : in  std_logic;
+
+      key_left  : in  std_logic;
+      key_up    : in  std_logic;
+      key_down  : in  std_logic;
+      key_right : in  std_logic;
+
+      cell00 : out std_logic_vector(3 downto 0);
+      cell01 : out std_logic_vector(3 downto 0);
+      cell02 : out std_logic_vector(3 downto 0);
+      cell03 : out std_logic_vector(3 downto 0);
+      cell10 : out std_logic_vector(3 downto 0);
+      cell11 : out std_logic_vector(3 downto 0);
+      cell12 : out std_logic_vector(3 downto 0);
+      cell13 : out std_logic_vector(3 downto 0);
+      cell20 : out std_logic_vector(3 downto 0);
+      cell21 : out std_logic_vector(3 downto 0);
+      cell22 : out std_logic_vector(3 downto 0);
+      cell23 : out std_logic_vector(3 downto 0);
+      cell30 : out std_logic_vector(3 downto 0);
+      cell31 : out std_logic_vector(3 downto 0);
+      cell32 : out std_logic_vector(3 downto 0);
+      cell33 : out std_logic_vector(3 downto 0)
+    );
+  end component;
 
   signal red_int, green_int, blue_int : std_logic_vector(7 downto 0);
   signal vga_r_int, vga_g_int, vga_b_int : std_logic_vector(7 downto 0);
@@ -121,7 +167,12 @@ end component;
   signal grid_r, grid_g, grid_b : std_logic_vector(7 downto 0);
   signal tile_r, tile_g, tile_b : std_logic_vector(7 downto 0);
   signal tile_on_int : std_logic;
-  
+
+  signal cell00_s, cell01_s, cell02_s, cell03_s : std_logic_vector(3 downto 0);
+  signal cell10_s, cell11_s, cell12_s, cell13_s : std_logic_vector(3 downto 0);
+  signal cell20_s, cell21_s, cell22_s, cell23_s : std_logic_vector(3 downto 0);
+  signal cell30_s, cell31_s, cell32_s, cell33_s : std_logic_vector(3 downto 0);
+
 begin
 
   VGA_HS <= horiz_sync_int;
@@ -156,19 +207,101 @@ begin
       Green        => grid_g,
       Blue         => grid_b
     );
-	 
-	U3: tiles_2048
-	  port map(
-		 pixel_row    => pixel_row_int,
-		 pixel_column => pixel_column_int,
-		 tile_on      => tile_on_int,
-		 Red          => tile_r,
-		 Green        => tile_g,
-		 Blue         => tile_b
-	  );
-	  
+
+  U3: movement_2048
+    port map(
+      clk       => CLOCK_50,
+      reset_n   => SW(17),
+
+      key_left  => KEY(3),
+      key_up    => KEY(2),
+      key_down  => KEY(1),
+      key_right => KEY(0),
+
+      cell00 => cell00_s,
+      cell01 => cell01_s,
+      cell02 => cell02_s,
+      cell03 => cell03_s,
+      cell10 => cell10_s,
+      cell11 => cell11_s,
+      cell12 => cell12_s,
+      cell13 => cell13_s,
+      cell20 => cell20_s,
+      cell21 => cell21_s,
+      cell22 => cell22_s,
+      cell23 => cell23_s,
+      cell30 => cell30_s,
+      cell31 => cell31_s,
+      cell32 => cell32_s,
+      cell33 => cell33_s
+    );
+
+  U4: tiles_2048
+    port map(
+      pixel_row    => pixel_row_int,
+      pixel_column => pixel_column_int,
+
+      cell00 => cell00_s,
+      cell01 => cell01_s,
+      cell02 => cell02_s,
+      cell03 => cell03_s,
+      cell10 => cell10_s,
+      cell11 => cell11_s,
+      cell12 => cell12_s,
+      cell13 => cell13_s,
+      cell20 => cell20_s,
+      cell21 => cell21_s,
+      cell22 => cell22_s,
+      cell23 => cell23_s,
+      cell30 => cell30_s,
+      cell31 => cell31_s,
+      cell32 => cell32_s,
+      cell33 => cell33_s,
+
+      tile_on      => tile_on_int,
+      Red          => tile_r,
+      Green        => tile_g,
+      Blue         => tile_b
+    );
+
   red_int   <= tile_r when tile_on_int = '1' else grid_r;
   green_int <= tile_g when tile_on_int = '1' else grid_g;
   blue_int  <= tile_b when tile_on_int = '1' else grid_b;
+
+  -- Unused outputs
+  SMA_CLKOUT <= '0';
+
+  UART_CTS <= '0';
+  UART_TXD <= '1';
+
+  LCD_BLON <= '0';
+  LCD_EN   <= '0';
+  LCD_ON   <= '0';
+  LCD_RS   <= '0';
+  LCD_RW   <= '0';
+  LCD_DATA <= (others => 'Z');
+
+  SRAM_ADDR <= (others => '0');
+  SRAM_DQ   <= (others => 'Z');
+  SRAM_CE_N <= '1';
+  SRAM_LB_N <= '1';
+  SRAM_OE_N <= '1';
+  SRAM_UB_N <= '1';
+  SRAM_WE_N <= '1';
+
+  AUD_DACDAT <= '0';
+  AUD_XCK    <= '0';
+
+  HEX0 <= (others => '1');
+  HEX1 <= (others => '1');
+  HEX2 <= (others => '1');
+  HEX3 <= (others => '1');
+  HEX4 <= (others => '1');
+  HEX5 <= (others => '1');
+  HEX6 <= (others => '1');
+  HEX7 <= (others => '1');
+
+  LEDG <= (others => '0');
+  LEDR <= (others => '0');
 
 end structural;
--- tiles_2048.vhd
+++ tiles_2048.vhd
@@ -6,10 +6,28 @@ entity tiles_2048 is
   port(
     pixel_row    : in  std_logic_vector(10 downto 0);
     pixel_column : in  std_logic_vector(10 downto 0);
-    tile_on      : out std_logic;
-    Red          : out std_logic_vector(7 downto 0);
-    Green        : out std_logic_vector(7 downto 0);
-    Blue         : out std_logic_vector(7 downto 0)
+
+    cell00 : in std_logic_vector(3 downto 0);
+    cell01 : in std_logic_vector(3 downto 0);
+    cell02 : in std_logic_vector(3 downto 0);
+    cell03 : in std_logic_vector(3 downto 0);
+    cell10 : in std_logic_vector(3 downto 0);
+    cell11 : in std_logic_vector(3 downto 0);
+    cell12 : in std_logic_vector(3 downto 0);
+    cell13 : in std_logic_vector(3 downto 0);
+    cell20 : in std_logic_vector(3 downto 0);
+    cell21 : in std_logic_vector(3 downto 0);
+    cell22 : in std_logic_vector(3 downto 0);
+    cell23 : in std_logic_vector(3 downto 0);
+    cell30 : in std_logic_vector(3 downto 0);
+    cell31 : in std_logic_vector(3 downto 0);
+    cell32 : in std_logic_vector(3 downto 0);
+    cell33 : in std_logic_vector(3 downto 0);
+
+    tile_on : out std_logic;
+    Red     : out std_logic_vector(7 downto 0);
+    Green   : out std_logic_vector(7 downto 0);
+    Blue    : out std_logic_vector(7 downto 0)
   );
 end tiles_2048;
 
@@ -19,16 +37,47 @@ architecture behavior of tiles_2048 is
   constant CELL_SIZE  : integer := 120;
   constant GAP        : integer := 10;
 
-  -- one sample tile at row 0, col 0
-  constant TILE_ROW   : integer := 0;
-  constant TILE_COL   : integer := 0;
+  function get_cell_value(
+    r : integer;
+    c : integer;
+    cell00, cell01, cell02, cell03,
+    cell10, cell11, cell12, cell13,
+    cell20, cell21, cell22, cell23,
+    cell30, cell31, cell32, cell33 : std_logic_vector(3 downto 0)
+  ) return std_logic_vector is
+  begin
+    if    (r = 0 and c = 0) then return cell00;
+    elsif (r = 0 and c = 1) then return cell01;
+    elsif (r = 0 and c = 2) then return cell02;
+    elsif (r = 0 and c = 3) then return cell03;
+    elsif (r = 1 and c = 0) then return cell10;
+    elsif (r = 1 and c = 1) then return cell11;
+    elsif (r = 1 and c = 2) then return cell12;
+    elsif (r = 1 and c = 3) then return cell13;
+    elsif (r = 2 and c = 0) then return cell20;
+    elsif (r = 2 and c = 1) then return cell21;
+    elsif (r = 2 and c = 2) then return cell22;
+    elsif (r = 2 and c = 3) then return cell23;
+    elsif (r = 3 and c = 0) then return cell30;
+    elsif (r = 3 and c = 1) then return cell31;
+    elsif (r = 3 and c = 2) then return cell32;
+    else                         return cell33;
+    end if;
+  end function;
 
-  constant TILE_X     : integer := BOARD_X + TILE_COL * (CELL_SIZE + GAP);
-  constant TILE_Y     : integer := BOARD_Y + TILE_ROW * (CELL_SIZE + GAP);
 begin
 
-  process(pixel_row, pixel_column)
-    variable x, y : integer;
+  process(pixel_row, pixel_column,
+          cell00, cell01, cell02, cell03,
+          cell10, cell11, cell12, cell13,
+          cell20, cell21, cell22, cell23,
+          cell30, cell31, cell32, cell33)
+    variable x, y      : integer;
+    variable row_idx   : integer;
+    variable col_idx   : integer;
+    variable cell_x    : integer;
+    variable cell_y    : integer;
+    variable cell_val  : std_logic_vector(3 downto 0);
   begin
     x := to_integer(unsigned(pixel_column));
     y := to_integer(unsigned(pixel_row));
@@ -38,14 +87,35 @@ begin
     Green   <= (others => '0');
     Blue    <= (others => '0');
 
-    if (x >= TILE_X) and (x < TILE_X + CELL_SIZE) and
-       (y >= TILE_Y) and (y < TILE_Y + CELL_SIZE) then
-      tile_on <= '1';
+    if (x >= BOARD_X) and (x < BOARD_X + 4 * CELL_SIZE + 3 * GAP) and
+       (y >= BOARD_Y) and (y < BOARD_Y + 4 * CELL_SIZE + 3 * GAP) then
 
-      -- tile color for value 2
-      Red   <= x"EE";
-      Green <= x"E4";
-      Blue  <= x"DA";
+      col_idx := (x - BOARD_X) / (CELL_SIZE + GAP);
+      row_idx := (y - BOARD_Y) / (CELL_SIZE + GAP);
+
+      cell_x := (x - BOARD_X) mod (CELL_SIZE + GAP);
+      cell_y := (y - BOARD_Y) mod (CELL_SIZE + GAP);
+
+      if (col_idx >= 0 and col_idx <= 3 and row_idx >= 0 and row_idx <= 3) then
+        if (cell_x < CELL_SIZE) and (cell_y < CELL_SIZE) then
+          cell_val := get_cell_value(
+            row_idx, col_idx,
+            cell00, cell01, cell02, cell03,
+            cell10, cell11, cell12, cell13,
+            cell20, cell21, cell22, cell23,
+            cell30, cell31, cell32, cell33
+          );
+
+          if cell_val /= "0000" then
+            tile_on <= '1';
+
+            -- all live tiles same gray for now
+            Red   <= x"B8";
+            Green <= x"B8";
+            Blue  <= x"B8";
+          end if;
+        end if;
+      end if;
     end if;
   end process;
 
